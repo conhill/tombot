83981fa2ca99a40b6eeb2d8653491ff71e0c69af
Connor Hill <conhill@optonline.net>
2017-07-16 20:00:36 -0400

    testing changes

--- src/api/retweet.js
+++ src/api/retweet.js
@@ -1,6 +1,10 @@
 const Twit = require('twit')
 const unique = require('unique-random-array')
 const config = require('../config')
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
 // const request = require('request')
 var Sentencer = require('sentencer')
 var WordPOS = require('wordpos'),
